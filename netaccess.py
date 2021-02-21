5bcf547bc500ee0db85e930792db867e84a12caa
Koushik Kiran Kumar <ornk3.2104@gmail.com>
2021-02-21 08:49:26 +0000

    Added netaccess.py

---
+++ netaccess.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import selenium
+import selenium.webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+import os
+
+#--------------------------------firefox---------------------------------
+firefox_options = Options()
+firefox_options.add_argument("--headless") #Comment this to see the process
+browser = selenium.webdriver.Firefox(options=firefox_options)
+#browser = selenium.webdriver.Firefox()
+
+#--------------------------------chrome---------------------------------
+#(Uncomment below lines and comment above lines if you want to use chrome)
+ 
+#chrome_options = Options()
+#chrome_options.add_argument("--headless") #Remove this to see the process
+#browser = selenium.webdriver.Chrome(options=chrome_options)
+#browser = selenium.webdriver.Chrome()
+
+
+website = "https://netaccess.iitm.ac.in"
+user = "Roll Number" #Replace with your roll number
+pwd = "Password" #Replace with your password
+
+htmlElementUser = "userLogin"
+htmlElementPwd = "userPassword"
+htmlElementSubmit = "submit"
+htmlIdDuration = "radios-1"
+htmlElementApprove = "approveBtn"	
+
+try:
+	browser.get((website))
+	userElement = browser.find_element_by_name(htmlElementUser)
+	userElement.send_keys(user)		
+	pwdElement  = browser.find_element_by_name(htmlElementPwd)
+	pwdElement.send_keys(pwd)
+	signin  = browser.find_element_by_name(htmlElementSubmit)
+	signin.click()
+ 
+	browser.get((website + "/account/approve"))
+	durationElement = browser.find_element_by_id(htmlIdDuration)
+	durationElement.click()
+	approve  = browser.find_element_by_name(htmlElementApprove)
+	approve.click()
+ 
+	time.sleep(1)
+	browser.quit()
+
+	print("Netaccess Successful!")
+ 
+except Exception:
+	#### This exception occurs if the element are not found in the webpage.
+	print("Error!!!")
+	browser.quit()
+
+
+
+
